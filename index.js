0719e40e5e1ff461177e4a93b4e89e607f52ae39
bd-dev-ismail <ismailratul7@gmail.com>
2022-11-07 20:20:18 +0600

    final commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 //midlewares 
@@ -18,24 +19,48 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-
+function verifyJWT(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({ message: "Unauthorized Access!" });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.SCRECT_ACCESS_TOKEN, function(err, decoded){
+    if(err){
+      return res.status(403).send({ message: "Forbidded Access!" });
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 async function run(){
     try{
         const donationCollection = client.db('bloodDonation').collection('donation');
-        const eventsCollection = client.db('bloodDonation').collection('events')
+        const eventsCollection = client.db('bloodDonation').collection('events');
+        //jwt
+        app.post('/jwt', async(req, res)=> {
+         
+          const user = req.body;
+          const token = jwt.sign(user, process.env.SCRECT_ACCESS_TOKEN, {expiresIn: '10d'});
+          res.send({token})
+        })
         //create
         app.post("/donation", async (req, res) => {
           const doc = req.body;
-        //   console.log(doc);
+        
           const donation = await donationCollection.insertOne(doc);
           res.send(donation);
         });
         //read
         app.get("/donation", async(req,res)=> {
+          const page = parseInt(req.query.page);
+          const size = parseInt(req.query.size);
+          
           const query = {};
           const cursor = donationCollection.find(query);
-          const donation = await cursor.toArray();
-          res.send(donation);
+          const count = await donationCollection.estimatedDocumentCount();
+          const donation = await cursor.skip(page*size).limit(size).toArray();
+          res.send({count, donation});
         });
         //find one by id
         app.get('/donation/:id', async(req, res)=> {
@@ -46,12 +71,18 @@ async function run(){
         });
         //event api
         app.post('/events', async(req, res)=> {
+
           const event = req.body;
           const result = await eventsCollection.insertOne(event);
           res.send(result);
         });
         //email query !
-        app.get('/events', async(req, res)=> {
+        app.get('/events', verifyJWT, async(req, res)=> {
+          const decoded = req.decoded;
+          console.log(decoded);
+          if(decoded.email !== req.query.email){
+            return res.status(403).send({ message: "Forbidded Access!" });
+          }
           let query = {};
           if(req.query.email){
             query = {email: req.query.email}
@@ -61,10 +92,24 @@ async function run(){
           res.send(result);
         });
         //delete event
-        app.delete('/events/:id', async(req, res)=> {
+        app.delete('/events/:id',verifyJWT, async(req, res)=> {
           const id = req.params.id;
           const query = {_id: ObjectId(id)};
           const result = await eventsCollection.deleteOne(query);
+          res.send(result);
+        });
+        //update -status
+        app.patch('/events/:id',verifyJWT, async(req,res)=> {
+          
+          const id = req.params.id;
+          const filter = {_id: ObjectId(id)};
+          const status = req.body.status;
+          const updatedStatus = {
+            $set: {
+              status: status,
+            },
+          };
+          const result = await eventsCollection.updateOne(filter, updatedStatus);
           res.send(result);
         })
     }
